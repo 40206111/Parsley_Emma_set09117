fc3ee9eb4bb773f830f6b18c707580ee55a64573
Emma Parsley <40206111@live.napier.ac.uk>
2017-11-09 22:37:54 +0000

    Memory -Removed Unneeded variables in memory

--- Draughts/src/Memory.py
+++ Draughts/src/Memory.py
@@ -4,16 +4,10 @@ from Piece import Piece
 class Memory:
 
     def __init__(self, blackPieces, whitePieces):
-        self.blackPieces = blackPieces
-        self.whitePieces = whitePieces
-        self.allPieces = blackPieces + whitePieces
         self.usedPieces = []
         self.turn = 0
 
     def setPieces(self, blackPieces, whitePieces):
-        self.blackPieces = blackPieces
-        self.whitePieces = whitePieces
-        self.allPieces = blackPieces + whitePieces
         self.usedPieces.clear()
 
     def updateUsed(self, piece, turn):
